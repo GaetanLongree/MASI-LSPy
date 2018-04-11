dfcfee2c6990f5d642b1ef1052fad3305c6b8385
Gaetan Longree <gaetanlongree@gmail.com>
2018-04-11 19:00:56 +0200

    Updated data structures

--- data_structures.py
+++ data_structures.py
@@ -11,13 +11,17 @@ class Neighbor:
 		self.lastContact = datetime.utcnow()
 
 class LinkState:
-	def __init__(self, linkID, advRouter, seqNbr):
-		self.linkID = linkID
+	def __init__(self, advRouter, activeLinks, seqNbr):
 		self.advRouter = advRouter
+		self.activeLinks = activeLinks
 		self.seqNbr = seqNbr
 
 	def getSeqNbr(self):
 		return self.seqNbr
+
+	def updateActiveLinks(self, newActiveLinks, seqNbr):
+		self.activeLinks = newActiveLinks
+		self.seqNbr = seqNbr
 
 class NeighborsTable:
 	def __init__(self):
@@ -41,6 +45,12 @@ class NeighborsTable:
 		except KeyError:
                         print("ERROR: could not update dead timer for {0} - neighbor is not present in table".format(routerName))
 
+	def __str__(self):
+		toString = "###NEIGHBORS TABLES###\nNeighbor\tIP Address\t\tLast Contact\n"
+		for key, value in self.table.items():
+			toString += key + "\t\t" + value.ipAddress + "\t\t" + value.lastContact.strftime("%Y-%m-%d %H:%M:%S") + "\n"
+		return toString
+
 
 
 class LinkStateDatabase:
@@ -48,12 +58,38 @@ class LinkStateDatabase:
 		self.database = dict()
 		self.lock = thread.allocate_lock()
 
-	#def get(self, routerName):
-
 	#NB: routerActiveLinks is a dictionary of class dict
-	#def updateNeighbor(self, routerName, routerActiveLinks):
+	def insertEntries(self, routerName, routerActiveLinks, seqNbr):
+		self.database[routerName] = LinkState(routerName, routerActiveLinks, seqNbr)
 
-	#def updateNeighbor(self, routerName, routerActiveLinks):
+	def updateEntries(self, routerName, routerActiveLinks, seqNbr):
+		try:
+			self.database[routerName].updateActiveLinks(routerActiveLinks, seqNbr)
+		except KeyError:
+			print("ERROR: could not update neighbor {0} - neighbor is not present in database".format(routerName))
+
+	# returns true if routerName is present in the Neighbors Table, false otherwise
+	def contains(self, routerName):
+		try:
+			self.database[routerName]
+			return True
+		except KeyError:
+			return False
+
+	def getSeqNumber(self, routerName):
+		try:
+			return self.database[routerName].seqNbr
+		except KeyError:
+			# should not happen, returns number higher than max seq number to avoid updating
+			# non existant entry
+			print("ERROR: {0} is not in database - sequence number requested".format(routerName))
+			return 100
+	def __str__(self):
+		toString = "###LINK STATE DATABASE###\nAdvertising Router\tNeighboor\tLink Cost\n"
+		for key, value in self.database.items():
+			for subKey, subValue in value.activeLinks.items():
+				toString += key + "\t\t\t" + subKey + "\t\t" + subValue + "\n"
+		return toString
 
 
 # Global variables
