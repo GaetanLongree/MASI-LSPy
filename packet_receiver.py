91651ca4cf8b26b9bfb370b18a5eeaa80fbb32bb
Gaetan Longree <gaetanlongree@gmail.com>
2018-04-11 14:41:59 +0200

    Fixed indentation

--- packet_receiver.py
+++ packet_receiver.py
@@ -2,12 +2,12 @@ from scapy.all import *
 
 def packetTreatment(pktLoad):
 	pktArray = pktLoad.split()
-		if pktArray[0] == "HELLO":
-			# treat hello packet reception
-			print("Received a HELLO packet from {0}".format(pktArray[2]))
-		if pktArray[0] == "LSP":
-			# treat lsp packet reception
-			print("Received a LSP packet from {0}".format(pktArray[1]))
+	if pktArray[0] == "HELLO":
+		# treat hello packet reception
+		print("Received a HELLO packet from {0}".format(pktArray[2]))
+	if pktArray[0] == "LSP":
+		# treat lsp packet reception
+		print("Received a LSP packet from {0}".format(pktArray[1]))
 
 def packetCallback(pkt):
 	if UDP in pkt:
