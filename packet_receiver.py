bca6e6b09347ab7bd682140246547addb323530e
Gaetan Longree <gaetanlongree@gmail.com>
2018-04-14 22:17:56 +0200

    Added security for malformed LSP updates

--- packet_receiver.py
+++ packet_receiver.py
@@ -24,50 +24,57 @@ class PacketReceiverThread (threading.Thread):
 		#print(adjacencyTable)	# debug
 
 	def activeLinkPairsHandler(self, pktArray):
-		activeLinks = dict()
-		i = 3
-		while (i < len(pktArray)):
-			activeLinks[pktArray[i]] = pktArray[i+1]
-			i = i+2
-		return activeLinks
+		try:
+			activeLinks = dict()
+			i = 3
+			while (i < len(pktArray)):
+				activeLinks[pktArray[i]] = pktArray[i+1]
+				i = i+2
+			return activeLinks
+		except IndexError:
+			print("ERROR: bad LSP format received from {0} - ignoring update".format(pktArray[1]))
+			return None
 
 	def lspPacketHandler(self, pkt, pktArray):
 		# [0]LSP [1]Sender Name [2]Sequence Number [3..N]Adjacent Active Links in pairs [Neighbor Name, Link Cost]
 		# [3]Neighbor 1 Name [4]Link Cost to Neighbor 1 [5]Neighbor 2 Name [6]Link Cost to Neighbor 2 [...]
-		print("Received a LSP packet from {0}".format(pktArray[1]))
+		if pktArray[1] != config.routerName:
+			print("Received a LSP packet from {0}".format(pktArray[1]))
 
-		if adjacencyTable.contains(pktArray[1]):
-			# Convert Adjacent Active Links pair in 2 dimension table lsdUpdate[Neighbor][LinkCost]
-			activeLinks = self.activeLinkPairsHandler(pktArray)
-			if linkStateDatabase.contains(pktArray[1]):
-				if pktArray[2] > linkStateDatabase.getSeqNumber(pktArray[1]):
-					# Update existing entries in the LSDB
-					linkStateDatabase.acquire()
-					linkStateDatabase.updateEntries(pktArray[1], activeLinks, pktArray[2])
-					linkStateDatabase.release()
-					# forward received LSDU to all other neighbors
-			else:
-				# Insert new entries in the LSDB
-				linkStateDatabase.acquire()
-				linkStateDatabase.insertEntries(pktArray[1], activeLinks, pktArray[2])
-				linkStateDatabase.release()
-				# forward received LSDU to all other neighbors
-			# send LSACK to sender
-			sendLSAck(pktArray[1], pktArray[2])
-			# launch SPF recalculation
-			spf.run()
-		#print(linkStateDatabase)	# debug
+			if adjacencyTable.contains(pktArray[1]):
+				# Convert Adjacent Active Links pair in 2 dimension table lsdUpdate[Neighbor][LinkCost]
+				activeLinks = self.activeLinkPairsHandler(pktArray)
+				if activeLinks != None:
+					if linkStateDatabase.contains(pktArray[1]):
+						if pktArray[2] > linkStateDatabase.getSeqNumber(pktArray[1]):
+							# Update existing entries in the LSDB
+							linkStateDatabase.acquire()
+							linkStateDatabase.updateEntries(pktArray[1], activeLinks, pktArray[2])
+							linkStateDatabase.release()
+							# TODO forward received LSDU to all other neighbors
+					else:
+						# Insert new entries in the LSDB
+						linkStateDatabase.acquire()
+						linkStateDatabase.insertEntries(pktArray[1], activeLinks, pktArray[2])
+						linkStateDatabase.release()
+						# TODO forward received LSDU to all other neighbors
+					# send LSACK to sender
+					sendLSAck(pktArray[1], pktArray[2])
+					# launch SPF recalculation
+					spf.run()
+			#print(linkStateDatabase)	# debug
 
 
 	def lsackPacketHandler(self, pkt, pktArray):
 		# [0]LSACK [1]Sender Name [2]Sequence Number
-		print("Received a LSACK packet from {0}".format(pktArray[1]))
+		if pktArray[1] != config.routerName:
+			print("Received a LSACK packet from {0}".format(pktArray[1]))
 
-		if lSUSentTable.contains(pktArray[1]):
-			lSUSentTable.acquire()
-			lSUSentTable.deleteEntry(pktArray[1], int(pktArray[2]))
-			lSUSentTable.release()
-                #print(lSUSentTable)       # debug
+			if lSUSentTable.contains(pktArray[1]):
+				lSUSentTable.acquire()
+				lSUSentTable.deleteEntry(pktArray[1], int(pktArray[2]))
+				lSUSentTable.release()
+	                #print(lSUSentTable)       # debug
 
 	def dataPacketHandler(self, pkt, pktArray):
 		# [0]DATA [1]Sender Name [2]Destination Name [3...]Message
