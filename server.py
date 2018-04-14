14541038049e18dd079834df23b5f98aea2ea669
Gaetan Longree <gaetanlongree@gmail.com>
2018-04-14 19:39:05 +0200

    Added commands + added list of threads to launch

--- server.py
+++ server.py
@@ -22,11 +22,30 @@ print(adjacencyTable)
 
 t1 = threading.Thread(target=gestionOfLSDU(config))
 t1.start()
+
+# Thread a lancer:
+# - gestion de reception des packets
+#packetReceiverThread = PacketReceiverThread()
+#packetReceiverThread.start()
+# - gestion d'envoi des LSDU
+# - gestion d'envoi des HELLO
+# - monitoring des LSUSent
+
 # Boucle de lancement
 while True:
     s = input("Prompt>>")
-    if s == 'show ip ospf neighbor':
+    if s == 'show running-config' or s == 'show run':
+        print(config)
+    elif s == 'show ip ospf neighbor' or s == 'show neighbors':
         print(neighborsTable)
+    elif s == 'show ip ospf adjacency' or s == 'show adjacency':
+        print(adjacencyTable)
+    elif s == 'show ip ospf database' or s == 'show database' or s == 'show linkStateDatabase':
+        print(linkStateDatabase)
+    elif s == 'show ip ospf' or s == 'show spf':
+        print(spf)
+    elif s == 'show ip route' or s == 'show route':
+        print(routingTable)
     elif s == 'exit':
         print("Goodbye !")
         exit()
