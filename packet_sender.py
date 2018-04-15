6ad8f82e229d255849d224af9355164b0c5f90bf
Gaetan Longree <gaetanlongree@gmail.com>
2018-04-15 22:36:29 +0200

    Modified packet sender to Thread classes

--- packet_sender.py
+++ packet_sender.py
@@ -1,44 +1,87 @@
 # here is the gestion of hello packet
 from scapy.all import *
 from data_structures import *
+import threading
 
-entier = 0
+class HelloHandlerThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stopThread = threading.Event()
 
-
-def gestionOfLSUSent():
-    for k in adjacencyTable:
-        sendSLU(k)
-    time.sleep(4 * config.maxLSPDelay)
-
-
-def sendSLU(adjacency):
-    lSUSent = LSUSentTable.insertLSUSent(adjacency, config.routerName, entier % 100, generatePayloadLSDU())
-    packet = IP(dst=adjacency.ipAddress) / UDP(sport=config.routerPort,
-                                               dport=adjacency.port) / Raw(load=lSUSent.payload)
-    send(packet)
-    #packet.show() # debug
-    t = threading.Thread(target=checkSLUTable(lSUSent))
-    t.start()
-
-
-def checkSLUTable(lSUSent):
-    for x in range(0, 4):
-        time.sleep(config.maxLSPDelay)
-        if(LSUSentTable.contains(lSUSent)):
-            sendSLU(adjacencyTable.contains(lSUSent.routerName))
-        else:
-            break
-
-
-def sendHello():
-    #print("Sending HELLO message" # debug)
-    for key, value in neighborsTable.items():
-        msg = 'HELLO ' + config.routerName + ' ' + value.name
-        #print(msg) # debug
-        packet = IP(dst=value.ipAddress) / UDP(sport=config.routerPort,
+    def sendHello(self):
+        #print("Sending HELLO message" # debug)
+        for key, value in neighborsTable.items():
+            msg = 'HELLO ' + config.routerName + ' ' + value.name
+            #print(msg) # debug
+            packet = IP(dst=value.ipAddress) / UDP(sport=config.routerPort,
                                                dport=value.port) / Raw(load=msg)
-        #packet.show() # debug
-        send(packet)
+            #packet.show() # debug
+            send(packet)
+
+    def run(self):
+        while self.stopThread.isSet() is not True:
+            time.sleep(config.helloDelay)
+            sendHello
+
+    def stop(self):
+        self.stopThread.set()
+        super().join(timeout)
+
+
+class LSUHandlerThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stopThread = threading.Event()
+
+    def generatePayloadLSDU():
+        # messagesis:LSP[SenderName][SequenceNumber][AdjacentActive Links].
+        payload = "LSP " + config.routerName + " " + str(config.seqNbrInt % 100) + " "
+        for k in neighborsTable:
+            payload += neighborsTable[k].name + \
+                " " + neighborsTable[k].linkCost + " "
+        config.seqNbrInt += 1
+        print("payload of LSDU produce : " + payload)
+        return payload[:-1]
+
+    def run(self):
+        while self.stopThread.isSet() is not True:
+            print("BEGINING OF LSDU Gestion")
+            # print(config.maxLSPDelay)
+            payload = generatePayloadLSDU()
+            for k in adjacencyTable:
+                x = adjacencyTable[k]
+                if(x is not None):
+                    lsuSent = LSUSentTable.insertLSUSent(adjacency, config.routerName, config.seqNbrInt % 100, generatePayloadLSDU())
+                    packet = IP(dst=adjacency.ipAddress) / UDP(sport=config.routerPort, dport=adjacency.port) / Raw(load=lsuSent.payload)
+                    send(packet)
+                    lsuSentHandlerThread = LSUSentHandlerThread(lSUSent)
+                    lsuSentHandlerThread.start()
+                    # packet.show() # debug
+                else:
+                    print(k + "is gone")
+            time.sleep(config.maxLSPDelay)
+
+    def stop(self):
+        self.stopThread.set()
+        super().join(timeout)
+
+
+class LSUSentHandlerThread(threading.Thread):
+    def __init__(self, lsuSent):
+        threading.Thread.__init__(self)
+        self.stopThread = threading.Event()
+        self.lsuSent = lsuSent
+
+    def run(self):
+        for x in range(0, 4):
+            time.sleep(5)
+            if(LSUSentTable.contains(self.lsuSent)):
+                adjacency = adjacencyTable[self.lsuSent.routerName]
+                packet = IP(dst=adjacency.ipAddress) / UDP(sport=config.routerPort, dport=adjacency.port) / Raw(load=lsuSent.payload)
+                send(packet)
+            else:
+                break
+
 
 
 def sendLSAck(dstIP, dstPort, lspSenderName, seqNbr):
@@ -48,36 +91,6 @@ def sendLSAck(dstIP, dstPort, lspSenderName, seqNbr):
                                  dport=dstPort) / Raw(load=msg)
     # print(msg) #debug
     send(packet)
-
-
-def gestionOfLSDU():
-    while True:
-        print("BEGINING OF LSDU Gestion")
-        # print(config.maxLSPDelay)
-        payload = generatePayloadLSDU()
-        for k in adjacencyTable:
-            x = adjacencyTable[k]
-            if(x is not None):
-                packet = (IP(dst=x.ipAddress) / UDP(sport=config.routerPort,
-                                                    dport=x.port) / Raw(load=payload))
-            else:
-                print(k + "is gone")
-                send(packet)
-                # print("LSDU")
-                # packet.show() # debug
-        time.sleep(config.maxLSPDelay)
-
-
-def generatePayloadLSDU():
-    # messagesis:LSP[SenderName][SequenceNumber][AdjacentActive Links].
-    global entier
-    payload = "LSP " + config.routerName + " " + str(entier % 100) + " "
-    for k in neighborsTable:
-        payload += neighborsTable[k].name + \
-            " " + neighborsTable[k].linkCost + " "
-    entier += 1
-    print("payload of LSDU produce : " + payload)
-    return payload[:-1]
 
 
 def forwardLSDUToNeighbor(payload, routerName):
